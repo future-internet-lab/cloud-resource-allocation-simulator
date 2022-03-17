fcb4ce05dffc35aed1ff8bf18fd2041f716c6bf5
daohiep22 <85162681+daohiep22@users.noreply.github.com>
2022-03-17 11:24:12 +0700

    fix selector output
    fix selector output

--- sim/Selector.py
+++ sim/Selector.py
@@ -58,6 +58,7 @@ class SimpleSelector(Selector):
             vnfCap.append(vnf[1]["RAM"])
         
         alloc = randPlacement(serverCap, vnfCap)
+        print(alloc)
 
         if(alloc):
             deploy = {"node": [], "link": []}
@@ -91,9 +92,111 @@ class SimpleSelector(Selector):
                 
 
 
-import matplotlib.pyplot as plt
 
 class WaxmanSelector_0(Selector):
+    """
+    selector algorithm for analysing SFC waxman random topo
+    """
+    def __init__(self):
+        pass
+
+
+    def analyse(self, DC, sfc):
+        topo = copy.deepcopy(DC.topo)
+
+        def randPlacement(bin, package):
+            # print(bin)
+            # print(package)
+            n_bin = len(bin)
+            n_package = len(package)
+            # print("n_bin = ", n_bin)
+            # print("n_package = ", n_package)
+            arg = round(5 / 4 * pow(4 * n_bin, 2/3) + 1)
+            alloc = [-1] * n_package
+            for i in range(n_package):
+                for _ in range(0, 10):
+                    idx = np.random.randint(0, n_bin)
+                    # print(i, idx, bin[idx])
+                    if(bin[idx] >= package[i]):
+                        bin[idx] -= package[i]
+                        alloc[i] = idx + arg
+                        break
+                    if(_ == 9):
+                        return 0
+            return alloc
+
+        # alloc vnf to server
+        serverCap = []
+        for node in list(topo.nodes.data()):
+            if(node[1]["model"] == "server"):
+                serverCap.append(node[1]["RAM"][0] - node[1]["RAM"][1])
+        vnfCap = []
+        for vnf in list(sfc["struct"].nodes.data()):
+            vnfCap.append(vnf[1]["RAM"])
+
+        alloc = randPlacement(serverCap, vnfCap)
+        # print(serverCap)
+        # print(vnfCap)
+        # print("alloc =",alloc)
+
+        if(alloc):
+            # anaRes = {"node": [], "link": []}
+
+            for vnf in list(sfc["struct"].nodes.data()):
+                c_server = alloc[vnf[0]] # the choosen server
+                # deploy["node"].append([vnf[0], c_server])
+                vnf[1]["server"] = c_server
+
+            for (i, j) in sfc["struct"].edges:
+                s = sfc["struct"].nodes[i]["server"]
+                d = sfc["struct"].nodes[j]["server"]
+                # print(s, d)
+                _topo = copy.deepcopy(topo)
+                v_link = sfc["struct"].edges[i, j]
+                # print()
+                print("v_link = sfcbw", sfc["struct"].edges[i, j]["bw"], v_link["bw"])
+                for p_link in list(topo.edges.data()):
+                    # print(p_link[2]["bw"][1], p_link[2]['bw'][0])
+                    if(p_link[2]["bw"][0] - p_link[2]['bw'][1] < v_link["bw"]):
+                        _topo.remove_edge(p_link[0], p_link[1])
+                # if(len(topo.edges) != len(_topo.edges)):
+                #     print(len(topo.edges), len(_topo.edges))
+                try:
+                    route = nx.shortest_path(_topo, s, d)
+                    # print(route)
+                    for i in range(len(route) - 1):
+                        topo.edges[route[i], route[i+1]]['bw'] = [
+                            topo.edges[route[i], route[i+1]]['bw'][0],
+                            topo.edges[route[i], route[i+1]]['bw'][1] + v_link["bw"]
+                        ]
+
+                    sfc["struct"].edges[i, j]["bw"] = v_link["bw"]
+                    if(sfc["struct"].edges[i, j]["bw"] != v_link["bw"]):
+                        print("----------------------------after routing", sfc["struct"].edges[i, j]["bw"], v_link["bw"])
+                        exit()
+                    
+                    sfc["struct"].edges[i, j]["route"] = route
+                except:
+                    print(f"cannot routing from {s} to {d}")
+                    return False
+                # anaRes["link"].append({
+                #     "bw": v_link["bw"],
+                #     "route": route
+                # })
+                
+            
+            # anaRes["sfc"] = sfc
+            sfc["DataCentre"] = DC.id
+
+            return copy.deepcopy(sfc)
+
+        else:
+            return False
+
+
+
+
+class WaxmanSelector(Selector):
     """
     selector algorithm for analysing SFC waxman random topo
     """
@@ -185,7 +288,7 @@ class WaxmanSelector_0(Selector):
         # print(alloc)
 
         if(alloc):
-            deploy = {"node": [], "link": []}
+            # deploy = {"node": [], "link": []}
             """temp = list(nx.connected_components(sfc["struct"]))
             print(temp)
             for i in temp:
@@ -193,12 +296,12 @@ class WaxmanSelector_0(Selector):
 
             for vnf in list(sfc["struct"].nodes.data()):
                 c_server = alloc[vnf[0]] # the choosen server
-                deploy["node"].append([vnf[0], c_server])
-                vnf[1]["place"] = [DC.id, c_server]
+                # deploy["node"].append([vnf[0], c_server])
+                vnf[1]["server"] = c_server
 
             for (i, j) in sfc["struct"].edges:
-                s = sfc["struct"].nodes[i]["place"][1]
-                d = sfc["struct"].nodes[j]["place"][1]
+                s = sfc["struct"].nodes[i]["server"]
+                d = sfc["struct"].nodes[j]["server"]
                 _topo = fat_tree(round((len(serverCap)*4)**(1/3)))
                 if s == d: continue
                 v_link = sfc["struct"].edges[i, j]
@@ -212,18 +315,22 @@ class WaxmanSelector_0(Selector):
                             topo.edge[route[i],route[i+1]]['bw'][0],
                             topo.edge[route[i],route[i+1]]['bw'][1] + v_link["bw"]
                         ]
+                    sfc["struct"].edges[i, j]["bw"] = v_link["bw"]
+                    sfc["struct"].edges[i, j]["route"] = route
                 except:
                     return False
-                deploy["link"].append({
-                    "bw": v_link["bw"],
-                    "route": route
-                })
+                # deploy["link"].append({
+                #     "bw": v_link["bw"],
+                #     "route": route
+                # })
             
-            deploy["sfc"] = sfc
+            # deploy["sfc"] = sfc
             # print(deploy["sfc"]["id"])
             # exit()
+            
+            sfc["DataCentre"] = DC.id
 
-            return deploy
+            return copy.deepcopy(sfc)
             # deploy has format like deploy in formdata.py     
 
         else: return False
