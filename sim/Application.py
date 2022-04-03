4fc34d91120289d6d6d1d055a620b64042ec6cc1
Ma Duc <95759699+maduc238@users.noreply.github.com>
2022-04-03 13:20:46 +0700

    Add files via upload

--- sim/Application.py
+++ sim/Application.py
@@ -60,21 +60,22 @@ class WaxmanApp(Application):
 
     def create_SFC(self, id):
         n_VNFs = np.random.randint(self.arg[1][0], self.arg[1][1] + 1)
-        G = nx.waxman_graph(n_VNFs, self.arg[3][0], self.arg[3][1])
-
+        G = nx.waxman_graph(n_VNFs, self.arg[4][0], self.arg[4][1])
         for (i, j) in G.edges:
             G[i][j]['demand'] = np.random.randint(self.arg[2][0], self.arg[2][1] + 1)
             G[i][j]['route'] = []
+        demand = 0
         for i in range(n_VNFs):
             G.nodes[i]["SFC"] = id
-            G.nodes[i]["demand"] = 1
+            G.nodes[i]["demand"] = np.random.randint(self.arg[3][0], self.arg[3][1] + 1)
+            demand += G.nodes[i]["demand"]
             G.nodes[i]["server"] = False
             G.nodes[i].pop("pos", None)
         out_link = np.random.randint(self.arg[2][0], self.arg[2][1] + 1)
         time_to_live = round(np.random.exponential(scale=self.arg[0]))
         if time_to_live == 0: time_to_live = 1
 
-        return [G, out_link, time_to_live]
+        return [G, out_link, demand, time_to_live]
 
 
 
